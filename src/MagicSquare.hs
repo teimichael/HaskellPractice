753f26a1bbc3ef5d2b0271cbc78eb99393be6601
teimichael <teimichael@outlook.com>
2020-11-17 17:53:36 +0900

    Optimize code style.

--- src/MagicSquare.hs
+++ src/MagicSquare.hs
@@ -12,14 +12,14 @@ sumIs15 :: [Int] -> Bool
 sumIs15 xs = sum xs == 15
 
 groupByRow :: [Int] -> [[Int]]
-groupByRow xs = [snd (unzip (filter (\x -> (fst x) `div` sideLength == n) (zip [0..(sideLength^2-1)] xs))) | n <- [0..sideLength - 1]]
+groupByRow xs = [snd (unzip (filter (\x -> fst x `div` sideLength == n) (zip [0..(sideLength^2-1)] xs))) | n <- [0..sideLength - 1]]
 -- groupByRow xs = loop 0
 --     where   loop n  | n > sideLength - 1 = []
 --                     | otherwise = (row n):(loop (n + 1))
 --             row n = snd (unzip (filter (\x -> (fst x) `div` sideLength == n) (zip [0..(sideLength^2-1)] xs)))
 
 groupByCol :: [Int] -> [[Int]]
-groupByCol xs = [snd (unzip (filter (\x -> (fst x) `mod` sideLength == n) (zip [0..(sideLength^2-1)] xs))) | n <- [0..sideLength - 1]]
+groupByCol xs = [snd (unzip (filter (\x -> fst x `mod` sideLength == n) (zip [0..(sideLength^2-1)] xs))) | n <- [0..sideLength - 1]]
 -- groupByCol xs = loop 0
 --     where   loop n  | n > sideLength - 1 = []
 --                     | otherwise = (col n):(loop (n + 1))
@@ -41,7 +41,7 @@ groupByMainDiag xs = [diag, antidiag]
 
 ok :: [Int] -> Bool
 ok xs = all (\x -> sumIs15 x) (groupAll xs)
-    where   groupAll xs = (groupByCol xs)++(groupByRow xs)++(groupByMainDiag xs)
+    where   groupAll xs = groupByCol xs ++ groupByRow xs ++ groupByMainDiag xs
 
 magicSquare :: [[Int]]
 magicSquare = [xs | xs <- permutations[1..sideLength^2], ok xs]
@@ -50,7 +50,7 @@ magicSquare = [xs | xs <- permutations[1..sideLength^2], ok xs]
 -- Solution 2
 matrix :: [Int] -> [[Int]]
 matrix [] = []
-matrix vec = (take sideLength vec) : (matrix (drop sideLength vec))
+matrix vec = take sideLength vec : matrix (drop sideLength vec)
 
 allOk :: [Int] -> Bool
 allOk xs =  and $ map (==15) $ map (\x -> sum x) (groupByRow ++ groupByCol ++ [diag, antidiag])
@@ -66,14 +66,14 @@ magicSquare2 = [xs | xs <- permutations[1..sideLength^2], allOk xs]
 
 -- Solution 2'
 magicSquare2' :: [[Int]]
-magicSquare2' = filter (\x -> test $ matrix x) candidates
+magicSquare2' = filter (test . matrix) candidates
     where   test xs = and $ map (==15) $ map (\x -> sum x) (xs ++ transpose xs ++ [diag 0 xs, antiDiag 0 xs])
             diag n xs   | n > sideLength - 1 = []
-                        | otherwise = xs!!n!!n: (diag (n + 1) xs)
+                        | otherwise = xs!!n!!n: diag (n + 1) xs
             antiDiag n xs   | n > sideLength - 1 = []
-                            | otherwise = xs!!n!!(sideLength - 1 - n): (antiDiag (n + 1) xs)
+                            | otherwise = xs!!n!!(sideLength - 1 - n): antiDiag (n + 1) xs
             matrix [] = []
-            matrix vec = (take sideLength vec) : (matrix (drop sideLength vec))
+            matrix vec = take sideLength vec : matrix (drop sideLength vec)
             candidates = permutations [1..sideLength^2]
             
 
